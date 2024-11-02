9bfcdd38b6cbd4c0a70b1420021ad7515a3c3eb2
Tapas <tapasdash017@gmail.com>
2024-11-02 18:26:58 +0530

    feat: initial setup

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile page
+      </Link>
+    </View>
+  );
+}
