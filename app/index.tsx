186f1c2a65cc103088956d6d0de380a87493c0fc
Tapas <tapasdash017@gmail.com>
2024-11-08 03:36:03 +0530

    feat: added fonts

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center bg-zinc-50">
       <Text>Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
