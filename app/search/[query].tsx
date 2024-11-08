5698fbcd0fa6f200ed55a735f2f852c50c6f37b5
Tapas <tapasdash017@gmail.com>
2024-11-09 03:17:41 +0530

    feat: added search dynamic route like next.js

---
+++ app/search/[query].tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+//it is a part of dynamic route like next js
+
+const Search = () => {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default Search;
+
+const styles = StyleSheet.create({});
