aacee7867c6079e459335c8f868638a53cf4b767
Tapas <tapasdash017@gmail.com>
2024-11-10 04:08:02 +0530

    fix: letter spacing of pnr numbner

--- app/index.jsx
+++ app/index.jsx
@@ -10,7 +10,8 @@ const PnrStatus = () => {
     <SafeAreaProvider>
       <SafeAreaView className="flex-1 items-center justify-center">
         <TextInput
-          className="w-10/12 h-16 border-slate-900 border-2 rounded p-1"
+          className="w-10/12 h-16 border-slate-900 border-2 rounded p-3 tracking-widest"
+          // tracking-widest is for letter spacing
           value={pnrStatus}
           placeholder="  Enter your 10 digit PNR number"
           keyboardType="numeric"
