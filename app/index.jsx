84e0e1f9365b94ab3413e2433607fa233ccf4966
Tapas <tapasdash017@gmail.com>
2024-11-10 04:42:00 +0530

    feat: samlpe request in pnr

--- app/index.jsx
+++ app/index.jsx
@@ -5,6 +5,21 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 const PnrStatus = () => {
   const [pnrStatus, setPnrStatus] = useState("");
+  const handleGetPnrStatus = async () => {
+    try {
+      const response = await axios.post("your-api-endpoint", {
+        pnr: pnrNumber,
+      });
+
+      if (response.status === 200) {
+        navigation.navigate("PnrDetails", { data: response.data });
+      } else {
+        console.error("API request failed with status:", response.status);
+      }
+    } catch (error) {
+      console.error("Error fetching PNR status:", error);
+    }
+  };
   // const textInputMobileStyle = "w-10/12 h-16 border-slate-900 border-2 rounded";
   return (
     <SafeAreaProvider>
@@ -17,7 +32,10 @@ const PnrStatus = () => {
           keyboardType="numeric"
           onChangeText={(pnrStatus) => setPnrStatus(pnrStatus)}
         />
-        <Pressable className="bg-blue-500 w-10/12 p-3 mt-5 rounded ">
+        <Pressable
+          className="bg-blue-500 w-10/12 p-3 mt-5 rounded"
+          onPress={handleGetPnrStatus}
+        >
           <Text className="text-center">Get PNR Status</Text>
         </Pressable>
       </SafeAreaView>
